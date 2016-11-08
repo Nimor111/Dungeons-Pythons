8e2a1f3e9e2cdcf41b5454986a28c8afc2c62bae
Nimor111 <georgi.bojinov@hotmail.com>
2016-11-08 10:22:01 +0200

    Comitting Fights and a change to weapons

--- Fights.py
+++ Fights.py
@@ -1,2 +1,41 @@
+from Hero import Hero
+from Enemies import Enemy
+from Weapon import Weapon
+
+
 class Fights:
-    pass
+
+    def __init__(self, hero, enemy):
+        if type(hero) != Hero:
+            raise TypeError("Must be a hero!")
+        if type(enemy) != Enemy:
+            raise TypeError("Must be an enemy!")
+        self.hero = hero
+        self.hero.equip(Weapon())
+        self.enemy = enemy
+
+    def start_fight(self):
+        return " A fight has started between our Hero(health={}, mana={}) and \
+an Enemy(health={}, mana={}, damage={}) " \
+            .format(self.hero.get_health(), self.hero.get_mana(),
+                    self.enemy.get_health(), self.enemy.get_mana(),
+                    self.enemy.damage)
+
+    def hero_turn(self):
+
+
+    def enemy_turn(self):
+        pass
+
+    def fight(self):
+        pass
+
+
+def main():
+    fight = Fights(Hero(), Enemy())
+    print(fight.start_fight())
+    print(fight.hero_turn())
+
+
+if __name__ == "__main__":
+    main()
--- Weapon.py
+++ Weapon.py
@@ -3,3 +3,9 @@ class Weapon:
     def __init__(self, name="The Axe of Despair", damage=20):
         self.name = name
         self.damage = damage
+
+    def __str__(self):
+        return "Name: {}, Damage: {}".format(self.name, self.damage)
+
+    def __repr__(self):
+        return self.__str__()
