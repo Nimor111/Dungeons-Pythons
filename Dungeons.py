13b99857d17eac4fc7454f63391883bddd9d1d82
ivanmilevtues <ivanmilevtues@gmail.com>
2016-11-13 10:34:58 -0800

    some test + Dungeon almost done, Added test files

--- Dungeons.py
+++ Dungeons.py
@@ -1,3 +1,8 @@
+from random import randint
+from Hero import *
+# from Fights import *
+from Enemies import *
+
 MOVEMENT = {"right": (0, 1),
             "left": (0, -1),
             "up": (-1, 0),
@@ -6,15 +11,18 @@ MOVEMENT = {"right": (0, 1),
 
 
 class Dungeon:
-    def __init__(self, file_path):
-        with open(file_path, "r") as f:
+    def __init__(self, map_file, treasure_file):
+        with open(map_file, "r") as f:
             self.map = f.read().splitlines()
+        with open(treasure_file, "r") as f:
+            self.treasure_list = f.read().split(',')
 
     def print_map(self):
         for line in self.map_list:
             print("".join(line))
 
-    def spawn(self):
+    def spawn(self, hero):
+        self.hero = hero
         line_list = []
         new_line = ''
         for index in range(len(self.map)):
@@ -30,19 +38,41 @@ class Dungeon:
         self.map_list = [list(i) for i in self.map]
         return self.map
 
+    def pick_treasure(self):
+        return self.treasure_list[randint(0, len(self.treasure_list) - 1)]
+
     def validate(self, direction):
-        return MOVEMENT[direction][0] + self.j >= 0 and\
-               MOVEMENT[direction][0] + self.j < len(self.map) and\
-               MOVEMENT[direction][1] + self.i >= 0 and\
-               MOVEMENT[direction][1] + self.i < len(self.map[self.j]) and\
-               self.map_list[MOVEMENT[direction][0] + self.i][MOVEMENT[direction][1] + self.j] == '.' or \
-               self.map_list[MOVEMENT[direction][0] + self.i][MOVEMENT[direction][1] + self.j] == 'T'
+        # print("----------------------------------------")
+        # print(MOVEMENT[direction][0] + self.i >= 0)
+        # print(MOVEMENT[direction][0] + self.i < len(self.map))
+        # print(MOVEMENT[direction][1] + self.j)
+        # print(MOVEMENT[direction][1] + self.j < len(self.map[self.i - 1]))
+        return MOVEMENT[direction][0] + self.i >= 0 and\
+               MOVEMENT[direction][0] + self.i < len(self.map) and\
+               MOVEMENT[direction][1] + self.j >= 0 and\
+               MOVEMENT[direction][1] + self.j < len(self.map[self.i - 1]) and\
+               (self.map_list[MOVEMENT[direction][0] + self.i][MOVEMENT[direction][1] + self.j] == '.' or
+                self.map_list[MOVEMENT[direction][0] + self.i][MOVEMENT[direction][1] + self.j] == 'T' or
+                self.map_list[MOVEMENT[direction][0] + self.i][MOVEMENT[direction][1] + self.j] == 'E')
 
     def move_hero(self, direction):
+        print(self.validate(direction))
         if self.validate(direction):
             self.map_list[self.i][self.j] = '.'
             self.i += MOVEMENT[direction][0]
             self.j += MOVEMENT[direction][1]
+            print(str(self.i) + ' ' + str(self.j))
+            if self.map_list[self.i][self.j] == 'T':
+                pot = self.pick_treasure()
+                print("Found " + pot + "potion.")
+                if pot == "health":
+                    self.hero.take_healing(10)
+                    # Should take health value from the file
+                else:
+                    # Should take health value from the file
+                    self.hero.take_mana(10)
+            elif self.map_list[self.i][self.j] == 'E':
+                pass
             self.map_list[self.i][self.j] = 'H'
             return True
         else:
--- test_Dungeon.py
+++ test_Dungeon.py
@@ -1,26 +1,32 @@
 import unittest
-from Dungeons import Dungeon
+from Dungeons import *
 
 
 class testDungeon(unittest.TestCase):
     def setUp(self):
-        self.d1 = Dungeon("test.txt")
+        self.d1 = Dungeon("test1.txt", "treasures.txt")
 
     def test_spawn(self):
-        self.assertEqual(self.d1.spawn(), ["H.##.....T",
-                                           "#T##..###.",
-                                           "#.###E###E",
-                                           "#.E...###.",
-                                           "###T#####G"])
+        self.h = Hero("this", "that", 100, 100, 2)
+        self.assertEqual(self.d1.spawn(self.h), ["H.##.....T",
+                                                 "#T##..###.",
+                                                 "#.###E###E",
+                                                 "#.E...###.",
+                                                 "###T#####G"])
 
     def test_validate(self):
-        self.d1.spawn()
-        self.assertFalse(self.d1.validate("left"))
+        self.h = Hero("this", "that", 100, 100, 2)
+        # self.d1.spawn(self.h)
+        # self.assertFalse(self.d1.validate("left"))
+        # self.assertTrue(self.d1.validate("right"))
+        # self.assertFalse(self.d1.validate("up"))
+        # self.assertFalse(self.d1.validate("down"))
+        self.d1.i = 1
+        self.d1.j = 0
+        self.assertTrue(self.d1.validate("left"))
         self.assertTrue(self.d1.validate("right"))
         self.assertFalse(self.d1.validate("up"))
-        self.assertFalse(self.d1.validate("down"))
-
-    def test_movement(self):
+        self.assertTrue(self.d1.validate("down"))
 
 if __name__ == '__main__':
     unittest.main()
